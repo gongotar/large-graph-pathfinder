4d151b543a712af0c814f0a491f1841782f2c7fe
gongotar <masood.gholami@aol.com>
2015-12-24 23:48:21 +0100

    class main

---
+++ src/main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Masoud Gholami
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
